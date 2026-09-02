3e792068fe8d22ee42391bdbc38777f73b8e8478
sanjana Mudduluru <sanjana3693@gmail.com>
2026-09-01 21:56:17 -0500

    Initial commit

---
+++ src/Java1Review.java
@@ -0,0 +1,14 @@
+/**
+ * Implement each of the 10 methods tested in JUnitTests.java. Study the tests
+ * to determine how the methods should work.
+ */
+public class Java1Review {
+
+	public static void main(String[] args) {
+		// If you want to write your own tests, do so here. (Do not modify
+		// JUnitTests.java.) To run this method in Eclipse, right-click
+		// Java1Review.java in the Package Explorer and select "Run As" >
+		// "Java Application" from the context menu.
+	}
+
+}
